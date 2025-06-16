0ae1f92d43b6194d6d4866e88bb90197bc6de555
harpreet-2146 <harpreetk2146@gmail.com>
2025-06-16 21:59:26 +0530

    categories done

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -9,14 +9,15 @@ const MainBanner=()=>{
             alt="banner" className="w-full hidden md:block"/>
             <img src={assets.main_banner_bg_sm} 
             alt="banner" className="w-full md:hidden"/>
-            <div>
+            <div className="absolute inset-0 flex flex-col items-center md:items-start
+            justify-end md:justify-center pd-24 md:pb-0 px-4 md:pl-18 lg:pl-24" >
                 <h1 className="text-3xl md:text-4xl lg:text-5xl
                 font-bold text-center md:text-left max-w-72
                 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">Freshness You Can Trust,
                     Savings you Will Love! </h1>
-            </div>
+            
 
-            <div className="flex items-center mt-6 font-mdeium">
+            <div className="flex items-center mt-6 font-medium">
                 <Link to={"/products"}
                 className="group flex items-center gap-2 px-7 md:px-9
                 py-3 bg-primary hover:bg-primary-dull transition rounded
@@ -32,6 +33,7 @@ const MainBanner=()=>{
                  src={assets.black_arrow_icon}
                 alt="arrow"/>
                 </Link>
+            </div>
             </div>
         </div>
     )
