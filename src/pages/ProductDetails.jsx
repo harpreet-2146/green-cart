15ea89b4491044bb63d4b4992939764f65609358
harpreet-2146 <harpreetk2146@gmail.com>
2025-06-21 22:10:01 +0530

    related products

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useAppContext } from '../context/AppContext';
 import { Link, useParams } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import ProductCard from '../components/ProductCard'
 
 const ProductDetails = () => {
     
@@ -81,11 +82,28 @@ const ProductDetails = () => {
                         </button>
                         <button onClick={()=>{addToCart(product._id);
                             navigate("/cart")
-                        }} className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-indigo-600 transition" >
+                        }} className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition" >
                             Buy now
                         </button>
                     </div>
                 </div>
+            </div>
+            {/*related products*/}
+            <div className='flex flex-col items-center mt-20'>
+               <div className='flex flex-col items-center w-max'>
+                <p className='text-3xl font-medium'>Related Products</p>
+                <div className='w-20 h-0.5 bg-primary rounded-full mt-2'></div>
+               </div>
+               <div className='grid grid-cols-2 sm:grid-cols-3 md:grids-cols-4
+               gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full'>
+                {relatedProducts.filter((product)=>product.inStock)
+                .map((product,index)=>(
+                    <ProductCard key={index} product={product}/>
+                ))}
+               </div>
+               <button onClick={()=>{navigate('/products'); scrollTo(0,0)}}
+               className='mx-auto cursor-pointer px-12 my-16 py-2.5
+               border rounded text-primary hover:bg-primary/10 transition'>See More</button>
             </div>
         </div>
     );
